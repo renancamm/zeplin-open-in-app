4cf3fecddcf9fb0da06f889e723ded761a1769bb
Renan Cammarosano <beik@Renans-MBP.connect>
2018-08-08 02:43:58 +0100

    add settings

--- background.js
+++ background.js
@@ -1,27 +1,45 @@
-var callback = function(details) {
+var settingEnabled
+chrome.storage.sync.get({ settingEnabled: true },
+    function (items) { settingEnabled = items.settingEnabled; }
+);
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (key in changes) {
+        settingEnabled = changes[key].newValue;
+    }
+});
+
+
+var callback = function (details) {
     console.log(details);
+    if (settingEnabled) {
+        console.log("settingEnable is on: " + settingEnabled);
 
-    //get ids from url string
-    project_id = details.url.match(/(?<=project\/)(.*?)(?=\/|\?|$)/g);
-    screen_id = details.url.match(/(?<=screen\/)(.*?)(?=\/|\?|$)/g);
-    console.log("pid: "+project_id)
-    console.log("sid: "+screen_id)
+        //get ids from url string
+        project_id = details.url.match(/(?<=project\/)(.*?)(?=\/|\?|$)/g);
+        screen_id = details.url.match(/(?<=screen\/)(.*?)(?=\/|\?|$)/g);
+        console.log("pid: " + project_id)
+        console.log("sid: " + screen_id)
 
-    //use project or screen url?
-    if (project_id != null) {
-        if (screen_id != null) {
-        app_url = "zpl://screen?sid="+screen_id+"&pid="+project_id; //use screen url
+        //use project or screen url?
+        if (project_id != null) {
+            if (screen_id != null) {
+                app_url = "zpl://screen?sid=" + screen_id + "&pid=" + project_id; //use screen url
+            }
+            else {
+                app_url = "zpl://project?pid=" + project_id; //use project url
+            }
+            console.log("redirect: " + app_url);
+            return { redirectUrl: app_url }; //Redirect
+        } else {
+            return //Skip if no project id
         }
-        else {
-            app_url = "zpl://project?pid="+project_id ; //use project url
-        }
-        console.log("redirect: "+app_url);
-        return {redirectUrl: app_url }; //Redirect
     } else {
-        return //Skip if no project id
+        console.log("settingEnable is off: " + settingEnabled);
     }
 };
-var filter = { urls: ["https://app.zeplin.io/project/*" ] };
+
+var filter = { urls: ["https://app.zeplin.io/project/*"] };
 var opt_extraInfoSpec = ["blocking"];
 
 chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
