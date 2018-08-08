6b129681811da186b5c5522414d6da16a9806879
Renan Cammarosano <beik@Renans-MBP.connect>
2018-08-08 22:49:14 +0100

    add components support

--- background.js
+++ background.js
@@ -28,29 +28,39 @@ var requestCallback = function (details) {
         // Get ids from url string
         project_path_id = details.url.match(/(?<=project\/)(.*?)(?=\/|\?|$)/g);
         screen_path_id = details.url.match(/(?<=screen\/)(.*?)(?=\/|\?|$)/g);
-        screen_query_id = details.url.match(/(?<=sid\=)(.*?)(?=\/|\?|$)/g);
-        section_query_id = details.url.match(/(?<=seid\=)(.*?)(?=\/|\?|$)/g);
-        tag_query_id = details.url.match(/(?<=tag\=)(.*?)(?=\/|\?|$)/g);
+        screen_query_id = details.url.match(/(?<=\?sid\=)(.*?)(?=\/|\?|$)/g);
+        section_query_id = details.url.match(/(?<=\?seid\=)(.*?)(?=\/|\?|$)/g);
+        tag_query_id = details.url.match(/(?<=\?tag\=)(.*?)(?=\/|\?|$)/g);
+        component_screen_query_id = details.url.match(/(?<=\?coid\=)(.*?)(?=\/|\?|$)/g);
+        component_section_query_id = details.url.match(/(?<=\?cseid\=)(.*?)(?=\/|\?|$)/g);
 
-        // Open an project, screen, section o tag view?
+
+        // Open an project, screen, section, tag or component view?
         if (project_path_id) {
             if (screen_path_id) {
-                app_url = "zpl://screen?sid=" + screen_path_id + "&pid=" + project_path_id; // Build screen url
+                app_url = "zpl://screen?sid="+screen_path_id+"&pid="+project_path_id;
             }
             else if (screen_query_id) {
-                app_url = "zpl://screen?sid=" + screen_query_id + "&pid=" + project_path_id; // Build screen url
+                app_url = "zpl://screen?sid="+screen_query_id+"&pid="+project_path_id;
             }
             else if (section_query_id) {
-                app_url = "zpl://project?pid=" + project_path_id + "&seid=" + section_query_id; // Build section url
+                app_url = "zpl://project?pid="+project_path_id+"&seid="+section_query_id;
             }
             else if (tag_query_id) {
-                app_url = "zpl://project?pid=" + project_path_id + "&tag=" + tag_query_id; // Build tag url
+                app_url = "zpl://project?pid="+project_path_id+"&tag="+tag_query_id;
+            }
+            else if (component_screen_query_id) {
+                app_url = "zpl://components?pid="+project_path_id+"&coids="+component_screen_query_id;
+            }
+            else if (component_section_query_id) {
+                app_url = "zpl://components?pid="+project_path_id+"&cseid="+component_section_query_id;
             }
             else {
-                app_url = "zpl://project?pid=" + project_path_id; // Build project url
+                app_url = "zpl://project?pid="+project_path_id;
             }
             closeTab(details.tabId);
-            return { redirectUrl: app_url }  // Redirect
+            console.log(app_url);
+            return { redirectUrl: app_url }  // Redirect with zpl protocol
         }
     }
 };
