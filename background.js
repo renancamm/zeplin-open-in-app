fa46d8b501f8627fcbea27aa21cc2a0f16ec4ebf
Renan Cammarosano <beik@Renans-MBP.connect>
2018-08-08 22:07:06 +0100

    add tag view support

--- background.js
+++ background.js
@@ -22,7 +22,6 @@ var storageCallback = function (changes, namespace) {
 }
 
 var requestCallback = function (details) {
-    console.log(details);
     // Check if is enable
     if (settingEnabled) {
 
@@ -31,8 +30,9 @@ var requestCallback = function (details) {
         screen_path_id = details.url.match(/(?<=screen\/)(.*?)(?=\/|\?|$)/g);
         screen_query_id = details.url.match(/(?<=sid\=)(.*?)(?=\/|\?|$)/g);
         section_query_id = details.url.match(/(?<=seid\=)(.*?)(?=\/|\?|$)/g);
+        tag_query_id = details.url.match(/(?<=tag\=)(.*?)(?=\/|\?|$)/g);
 
-        // Use project or screen url?
+        // Open an project, screen, section o tag view?
         if (project_path_id) {
             if (screen_path_id) {
                 app_url = "zpl://screen?sid=" + screen_path_id + "&pid=" + project_path_id; // Build screen url
@@ -42,6 +42,9 @@ var requestCallback = function (details) {
             }
             else if (section_query_id) {
                 app_url = "zpl://project?pid=" + project_path_id + "&seid=" + section_query_id; // Build section url
+            }
+            else if (tag_query_id) {
+                app_url = "zpl://project?pid=" + project_path_id + "&tag=" + tag_query_id; // Build tag url
             }
             else {
                 app_url = "zpl://project?pid=" + project_path_id; // Build project url
