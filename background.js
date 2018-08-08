082f37b7ac2da1cdebb9f3b328cf98f68da0d53e
Renan Cammarosano <beik@Renans-MBP.connect>
2018-08-08 21:51:15 +0100

    improve url screen match

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-//Sync with storage on init
+// Sync with storage on init
 
 var settingEnabled
 chrome.storage.sync.get({ settingEnabled: true },
@@ -12,50 +12,43 @@ chrome.storage.sync.get({ settingEnabled: true },
 
 
 
-//Callbacks
+// Callbacks
 
 var storageCallback = function (changes, namespace) {
     for (key in changes) {
         settingEnabled = changes[key].newValue;
-        console.log("Updated settings: " + settingEnabled);
         updateBadge();
     }
 }
 
 var requestCallback = function (details) {
     console.log(details);
-    //check if is enable
+    // Check if is enable
     if (settingEnabled) {
-        console.log("settingEnable is on: " + settingEnabled);
 
-        //get ids from url string
-        project_id = details.url.match(/(?<=project\/)(.*?)(?=\/|\?|$)/g);
-        screen_id = details.url.match(/(?<=screen\/)(.*?)(?=\/|\?|$)/g);
-        section_id = details.url.match(/(?<=seid\=)(.*?)(?=\/|\?|$)/g);
+        // Get ids from url string
+        project_path_id = details.url.match(/(?<=project\/)(.*?)(?=\/|\?|$)/g);
+        screen_path_id = details.url.match(/(?<=screen\/)(.*?)(?=\/|\?|$)/g);
+        screen_query_id = details.url.match(/(?<=sid\=)(.*?)(?=\/|\?|$)/g);
+        section_query_id = details.url.match(/(?<=seid\=)(.*?)(?=\/|\?|$)/g);
 
-        console.log("pid: " + project_id)
-        console.log("sid: " + screen_id)
-        console.log("seid: " + section_id)
-
-        //use project or screen url?
-        if (project_id) {
-            if (screen_id) {
-                app_url = "zpl://screen?sid=" + screen_id + "&pid=" + project_id; //use screen url
+        // Use project or screen url?
+        if (project_path_id) {
+            if (screen_path_id) {
+                app_url = "zpl://screen?sid=" + screen_path_id + "&pid=" + project_path_id; // Build screen url
             }
-            else if (section_id) {
-                app_url = "zpl://project?pid=" + project_id + "&seid=" + section_id; //use section url
+            else if (screen_query_id) {
+                app_url = "zpl://screen?sid=" + screen_query_id + "&pid=" + project_path_id; // Build screen url
+            }
+            else if (section_query_id) {
+                app_url = "zpl://project?pid=" + project_path_id + "&seid=" + section_query_id; // Build section url
             }
             else {
-                app_url = "zpl://project?pid=" + project_id; //use project url
+                app_url = "zpl://project?pid=" + project_path_id; // Build project url
             }
-            console.log("redirect: " + app_url);
             closeTab(details.tabId);
-            return { redirectUrl: app_url }  //redirect
-        } else {
-            return //skip if no project id
+            return { redirectUrl: app_url }  // Redirect
         }
-    } else {
-        console.log("settingEnable is off: " + settingEnabled);
     }
 };
 
@@ -72,7 +65,6 @@ function updateBadge() {
         chrome.browserAction.setBadgeText({ text: "✕" });
         chrome.browserAction.setBadgeBackgroundColor({ color: "#da1616" });
     }
-
 }
 
 function closeTab(tabId) {
